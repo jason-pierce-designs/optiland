d1661fca248557ccd1018258584cab85fca70231
Jason Pierce <jason.pierce.designs@gmail.com>
2022-02-26 18:17:23 -0500

    fix: add vercel env var2

--- pages/collections/pbunny/[id].tsx
+++ pages/collections/pbunny/[id].tsx
@@ -10,10 +10,7 @@ import DarkOverlapShell from "../../../components/DarkOverlapShell";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const tokenId = context.params?.id as string;
-  const baseUrl =
-    process.env.VERCEL_URL ||
-    process.env.NEXT_PUBLIC_VERCEL_URL ||
-    process.env.NEXT_PUBLIC_BASEURL;
+  const baseUrl = process.env.NEXT_PUBLIC_BASEURL || "";
   const res: Response = await fetch(`${baseUrl}/api/meta/pbunny/${tokenId}`);
   const metadata: BunnyMetadata = await res.json();
   return {
