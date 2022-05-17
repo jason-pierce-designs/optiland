7a5ee23519c682465cc89a7e9aaad97f1acad5fa
Jason Pierce <jason.pierce.designs@gmail.com>
2022-05-16 23:34:00 -0400

    feat: update copy on blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -39,10 +39,10 @@ export default function BlogPage({ articles }: Props) {
           <div className="relative max-w-7xl mx-auto">
             <div className="text-center">
               <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-                Further Down The Rabbit Hole
+                Welcome to our Blog
               </h2>
               <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-                Got an idea for blog content? Let us know in our Discord server!
+                We try to update frequently. So check back often!
               </p>
             </div>
             <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
